31e7622d7ea6db1ece665f674df2ea1fab73ad8b
BERGERON-Alexandre-2225012aa <alexandrebergeron12@gmail.com>
2024-10-10 15:58:24 +0200

    Feat: mouvement de caméra et du joueur

--- Assets/script/RotateGold.cs
+++ Assets/script/RotateGold.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class rotate_gold : MonoBehaviour
+public class RotateGold : MonoBehaviour
 {
     // Vitesse de rotation en degrés par seconde
     public float rotationSpeed = 100f;
